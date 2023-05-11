bf4bb62b46aabf5c7fa185d9fd4c4b0ad3984287
BrayMurph <brayxmurph@gmail.com>
2023-05-11 17:19:36 -0400

    continuing to work on the js and html aspects of the coding project. So far able to get most of it working needing to finish the rest of the quiz and then work on the highscore chart afterwards

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,22 @@ var choiceB = document.getElementById("B");
 var choiceC = document.getElementById("C");
 var choiceD = document.getElementById("D");
 var timer = document.getElementById("timer");
+var counter = document.getElementById("counter");
 var score = document.getElementById("score");
 var highscore = document.getElementById("highscore");
+var answer = document.getElementById("answer");
+
+
+
+start.addEventListener("click", startQuiz);
+
+function startQuiz() {
+    start.style.display = "none";
+    renderQuestion();
+    quiz.style.display = "block";
+    startTimer();
+}
+
 
 var questions = [
     {
@@ -17,38 +31,38 @@ var questions = [
         choiceC : "Wrong",
         choiceD : "Wrong",
         correct : "A"
-    }
+    },
     {
         question : "What does X equal?",
-        choiceA : "Correct",
-        choiceB : "Wrong",
+        choiceA : "Wrong",
+        choiceB : "Correct",
         choiceC : "Wrong",
         choiceD : "Wrong",
-        correct : "A"
-    }
+        correct : "B"
+    },
     {
         question : "What does X equal?",
-        choiceA : "Correct",
+        choiceA : "Wrong",
         choiceB : "Wrong",
-        choiceC : "Wrong",
+        choiceC : "Correct",
         choiceD : "Wrong",
-        correct : "A"
-    }
+        correct : "C"
+    },
     {  
         question : "What does X equal?",
-        choiceA : "Correct",
+        choiceA : "Wrong",
         choiceB : "Wrong",
         choiceC : "Wrong",
-        choiceD : "Wrong",
-        correct : "A"
-    }
+        choiceD : "Correct",
+        correct : "D"
+    },
     {
         question : "What does X equal?",
         choiceA : "Correct",
         choiceB : "Wrong",
         choiceC : "Wrong",
-        choiceD : "Wrong",
-        correct : "A"
+        choiceD : "Correct",
+        correct : "D"
     }
 ];
 
@@ -66,6 +80,37 @@ function renderQuestion() {
     choiceD.innerHTML = q.choiceD;
 }
 
-start.style.display = "none";
-renderQuestion();
-quiz.style.display = "block";
+
+
+
+var countDown = 60;
+var timerDown;
+
+function startTimer() {
+    // Sets timer
+    timerDown = setInterval(function() {
+      counter.textContent = countDown;
+      countDown--;
+    }, 1000);
+  }
+  
+function checkAnswer(answer) {
+    if (answer == questions[runningQuestion].correct) {
+        answerIsCorrect();
+    } else {
+        answerIsWrong();
+        countDown = countDown - 5;
+    }
+    if(runningQuestion < lastQuestion) {
+        runningQuestion++;
+        renderQuestion();
+    }
+}
+
+function answerIsCorrect() {
+    answer = "Correct!"
+}
+
+function answerIsWrong() {
+    answer = "Wrong!"
+}
